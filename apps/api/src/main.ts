c01535ea35fa424a1e27c60f187c8bb8176fe4cc
irackson <nasirus@protonmail.com>
2021-09-11 17:27:55 -0400

    finish api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,27 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import { pokemon } from './pokemon';
 
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+    res.send({ message: 'Welcome to api!' });
+});
+
+app.get('/pokemon', (_, res) => {
+    res.send(pokemon);
+});
+
+app.get('/search', (req, res) => {
+    const q = ((req.query.q as string) ?? '').toLowerCase();
+    res.send(
+        pokemon.filter(({ name: { english } }) =>
+            english.toLowerCase().includes(q)
+        )
+    );
 });
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+    console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
