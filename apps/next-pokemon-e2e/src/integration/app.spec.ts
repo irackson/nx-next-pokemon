423ddf20f46eb3b2a8870b474cfc46094ff0c31e
Ian Rackson <75096000+irackson@users.noreply.github.com>
2021-09-12 02:02:18 -0400

    test

--- apps/next-pokemon-e2e/src/integration/app.spec.ts
+++ apps/next-pokemon-e2e/src/integration/app.spec.ts
@@ -7,6 +7,6 @@ describe('next-pokemon', () => {
         cy.get('input').first().type('bulb');
         cy.get('li').first().should('have.text', 'Bulbasaur');
         cy.get('body').screenshot();
-        console.log('yo');
+        console.log('yoaaaa');
     });
 });
