9e51c73b06c61d5ff650804e9ad7785db3268898
irackson <nasirus@protonmail.com>
2021-09-12 01:51:33 -0400

    add testing with cypress

--- apps/next-pokemon-e2e/src/integration/app.spec.ts
+++ apps/next-pokemon-e2e/src/integration/app.spec.ts
@@ -4,10 +4,8 @@ describe('next-pokemon', () => {
     beforeEach(() => cy.visit('/'));
 
     it('should display welcome message', () => {
-        // Custom command example, see `../support/commands.ts` file
-        cy.login('my-email@something.com', 'myPassword');
-
-        // Function helper example, see `../support/app.po.ts` file
-        getGreeting().contains('Welcome to next-pokemon!');
+        cy.get('input').first().type('bulb');
+        cy.get('li').first().should('have.text', 'Bulbasaur');
+        cy.get('body').screenshot();
     });
 });
